72735eb54ce0e5c3862ca92ad577a32ef507597c
hiwon-lee <84234411+hiwon-lee@users.noreply.github.com>
2024-11-21 19:44:51 +0900

    feat : cats provider

--- src/cats.controller.ts
+++ src/cats.controller.ts
@@ -7,14 +7,24 @@ import {
   Put,
   Param,
   Delete,
+  Res,
+  HttpStatus,
 } from '@nestjs/common';
+import { Response } from 'express';
+
 import { CreateCatDto } from './dto/create-cat.dto';
+import { CatsService } from './cats.service';
+import { Cat } from './interfaces/cat.interface';
 
 @Controller('cats')
 export class CatsController {
+  // 의존성 주입 - 디자인 패턴
+  constructor(private catsService: CatsService) {}
+
   @Post()
   create(@Body() createCatDto: CreateCatDto) {
-    return 'This action adds a new cat';
+    // return 'This action adds a new cat';
+    this.catsService.create(createCatDto);
   }
 
   //   @Get()
@@ -22,6 +32,14 @@ export class CatsController {
   //     return `This action returns all cats (limit: ${query.limit} items)`;
   //   }
 
+  //   @Get()
+  //   findAll(@Res() res: Response) {
+  //     res.status(HttpStatus.OK).json([]);
+  //   }
+  @Get()
+  async findAll(): Promise<Cat[]> {
+    return this.catsService.findAll();
+  }
   @Get(':id')
   findOne(@Param('id') id: string) {
     return `This action returns a #${id} cat`;
