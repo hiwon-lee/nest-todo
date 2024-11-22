74059b2f631ae2728e50b2d260cd704540801480
hiwon-lee <84234411+hiwon-lee@users.noreply.github.com>
2024-11-22 09:39:32 +0900

    fix :  cors error

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,16 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  // CORS 활성화
+  app.enableCors({
+    origin: 'http://localhost:3001', // React 앱의 주소
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // 허용할 HTTP 메서드
+    credentials: true, // 쿠키 사용을 허용하려면 true로 설정
+  });
+  // 글로벌 ValidationPipe 활성화
+  // app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
